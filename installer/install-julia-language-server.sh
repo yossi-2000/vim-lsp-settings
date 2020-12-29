be0fae82fd3f6dc851294e6b7bf02cf4f54832cf
Ryuhei Yoshida <yoshida_ryuuhei@yahoo.co.jp>
2020-12-30 01:28:31 +0900

    just move julia lsp

--- installer/install-julia-language-server.sh
+++ installer/install-julia-language-server.sh
@@ -4,10 +4,11 @@ set -e
 
 julia -e 'using Pkg; Pkg.add("LanguageServer")'
 
+ENVPATH='~/.julia/environments/v1.5'
 cat <<EOF >julia-language-server
 #!/usr/bin/env bash
 
-julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()" \$1
+julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()"  $ENVPATH
 EOF
 
 chmod +x julia-language-server
