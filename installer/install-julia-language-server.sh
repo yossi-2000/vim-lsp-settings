69c9d62db2c45348d705b37456372558b3c43038
Ryuhei Yoshida <yoshida_ryuuhei@yahoo.co.jp>
2020-12-30 01:01:48 +0900

    add environmental path of julia

--- installer/install-julia-language-server.sh
+++ installer/install-julia-language-server.sh
@@ -7,7 +7,7 @@ julia -e 'using Pkg; Pkg.add("LanguageServer")'
 cat <<EOF >julia-language-server
 #!/usr/bin/env bash
 
-julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()"
+julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()" \$1
 EOF
 
 chmod +x julia-language-server
